4921d21977560ce40a79ea732579187b11666e0b
929w268vzd-afk <929w268vzd@privaterelay.appleid.com>
2026-07-27 21:16:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,100 @@ opacity:0;
 `;
 
 document.head.appendChild(style);
+window.addEventListener("load",()=>{
+
+const loader=document.getElementById("loader");
+
+setTimeout(()=>{
+loader.style.display="none";
+},2000);
+
+});
+
+const button=document.querySelector(".loveBtn");
+
+if(button){
+
+button.addEventListener("click",()=>{
+
+alert(
+"❤️ Thank you for visiting this website.\n\nNo matter what your decision is,\nI will always respect you.\n\n- Pradeep ❤️"
+);
+
+});
+
+}
+
+const images=document.querySelectorAll(".gallery img");
+
+images.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+const overlay=document.createElement("div");
+
+overlay.style.position="fixed";
+overlay.style.left="0";
+overlay.style.top="0";
+overlay.style.width="100%";
+overlay.style.height="100%";
+overlay.style.background="rgba(0,0,0,.95)";
+overlay.style.display="flex";
+overlay.style.justifyContent="center";
+overlay.style.alignItems="center";
+overlay.style.zIndex="9999";
+
+const photo=document.createElement("img");
+
+photo.src=img.src;
+photo.style.maxWidth="90%";
+photo.style.maxHeight="90%";
+photo.style.borderRadius="20px";
+
+overlay.appendChild(photo);
+
+overlay.onclick=()=>overlay.remove();
+
+document.body.appendChild(overlay);
+
+});
+
+});
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.innerHTML="❤️";
+
+heart.style.position="fixed";
+heart.style.left=Math.random()*100+"vw";
+heart.style.top="100vh";
+heart.style.fontSize=(20+Math.random()*20)+"px";
+heart.style.pointerEvents="none";
+heart.style.zIndex="999";
+
+heart.animate(
+
+[
+{transform:"translateY(0)",opacity:1},
+{transform:"translateY(-120vh)",opacity:0}
+],
+
+{
+duration:5000+Math.random()*3000
+}
+
+);
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},8000);
+
+}
+
+setInterval(createHeart,700);
