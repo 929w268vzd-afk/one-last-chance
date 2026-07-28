290fa6c1c0c25599448fef692944582dbb547a04
929w268vzd-afk <929w268vzd@privaterelay.appleid.com>
2026-07-28 13:41:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -206,3 +206,26 @@ setTimeout(typeWriter,40);
 }
 
 typeWriter();
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+sections.forEach(section=>{
+
+section.classList.add("hidden");
+
+observer.observe(section);
+
+});
