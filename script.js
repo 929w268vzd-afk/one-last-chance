0380a7d1c336e9ba076ce77acd57041aa7415391
929w268vzd-afk <929w268vzd@privaterelay.appleid.com>
2026-07-28 13:32:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -163,3 +163,24 @@ musicBtn.innerHTML="🎵 Play Music";
 }
 
 };
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+function showSlide(){
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current>=slides.length){
+
+current=0;
+
+}
+
+slides[current].classList.add("active");
+
+}
+
+setInterval(showSlide,4000);
